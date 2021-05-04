a6d0986e499c674d778e2b962e41337d180a7761
Daiwery <andrey.luk10@gmail.com>
2021-05-04 22:23:48 +0300

    NN V2.

--- train/models/V2/V2.py
+++ train/models/V2/V2.py
@@ -5,43 +5,46 @@ import tensorflow as tf
 def createModel(dropout=True):
     inputMessage = tf.keras.Input(shape=(None, 2), name="inputMessage")
     inputValue = tf.keras.Input(shape=(None, 12), name="inputValue")
-    inputDT = tf.keras.Input(shape=(None, 21), name="inputDT")
+    inputDT = tf.keras.Input(shape=(None,), name="inputDT")
+    embeddingDT = tf.keras.layers.Embedding(21, 1)(inputDT)
 
-    concatenateInputDT = tf.keras.layers.concatenate([inputMessage, inputValue, inputDT], name="concatenateInputDT")
+    concatenateInputDT = tf.keras.layers.concatenate([inputMessage, inputValue, embeddingDT], name="concatenateInputDT")
     gruDT1 = tf.keras.layers.GRU(32, return_sequences=True, name="gruDT1")(concatenateInputDT)
     if dropout:
-        gruDT1 = tf.keras.layers.Dropout(0.2)(gruDT1)
+        gruDT1 = tf.keras.layers.Dropout(0.05)(gruDT1)
     gruDT2 = tf.keras.layers.GRU(32, return_sequences=True, name="gruDT2")(gruDT1)
     if dropout:
-        gruDT2 = tf.keras.layers.Dropout(0.2)(gruDT2)
+        gruDT2 = tf.keras.layers.Dropout(0.05)(gruDT2)
     concatenateOutputDT = tf.keras.layers.concatenate([gruDT1, gruDT2], name="concatenateOutputDT")
     outputDT = tf.keras.layers.Dense(21, activation="softmax", name="outputDT")(concatenateOutputDT)
 
-    concatenateInputMessage = tf.keras.layers.concatenate([inputMessage, inputValue, inputDT, outputDT],
+    concatenateInputMessage = tf.keras.layers.concatenate([inputMessage, inputValue, embeddingDT, outputDT],
                                                           name="concatenateInputMessage")
     gruMessage1 = tf.keras.layers.GRU(64, return_sequences=True, name="gruMessage1")(concatenateInputMessage)
     if dropout:
-        gruMessage1 = tf.keras.layers.Dropout(0.2)(gruMessage1)
+        gruMessage1 = tf.keras.layers.Dropout(0.05)(gruMessage1)
     gruMessage2 = tf.keras.layers.GRU(64, return_sequences=True, name="gruMessage2")(gruMessage1)
     if dropout:
-        gruMessage2 = tf.keras.layers.Dropout(0.2)(gruMessage2)
-    concatenateOutputMessage = tf.keras.layers.concatenate([gruMessage1, gruMessage2], name="concatenateOutputMessage")
+        gruMessage2 = tf.keras.layers.Dropout(0.05)(gruMessage2)
+    concatenateOutputMessage = tf.keras.layers.concatenate([outputDT, gruMessage1, gruMessage2],
+                                                           name="concatenateOutputMessage")
     outputMessage = tf.keras.layers.Dense(2, activation="softmax", name="outputMessage")(concatenateOutputMessage)
 
-    concatenateInputValue = tf.keras.layers.concatenate([inputMessage, inputValue, inputDT, outputDT, outputMessage],
-                                                        name="concatenateInputValue")
+    concatenateInputValue = tf.keras.layers.concatenate(
+        [inputMessage, inputValue, embeddingDT, outputDT, outputMessage], name="concatenateInputValue")
     gruValue1 = tf.keras.layers.GRU(128, return_sequences=True, name="gruValue1")(concatenateInputValue)
     if dropout:
-        gruValue1 = tf.keras.layers.Dropout(0.2)(gruValue1)
+        gruValue1 = tf.keras.layers.Dropout(0.05)(gruValue1)
     gruValue2 = tf.keras.layers.GRU(128, return_sequences=True, name="gruValue2")(gruValue1)
     if dropout:
-        gruValue2 = tf.keras.layers.Dropout(0.2)(gruValue2)
+        gruValue2 = tf.keras.layers.Dropout(0.05)(gruValue2)
     gruValue3 = tf.keras.layers.GRU(128, return_sequences=True, name="gruValue3")(gruValue2)
     if dropout:
-        gruValue3 = tf.keras.layers.Dropout(0.2)(gruValue3)
-    concatenateOutputValue = tf.keras.layers.concatenate([gruValue1, gruValue2, gruValue3],
+        gruValue3 = tf.keras.layers.Dropout(0.05)(gruValue3)
+    concatenateOutputValue = tf.keras.layers.concatenate([outputDT, outputMessage, gruValue1, gruValue2, gruValue3],
                                                          name="concatenateOutputValue")
     outputValue = tf.keras.layers.Dense(12, activation="softmax", name="outputValue")(concatenateOutputValue)
 
     return tf.keras.Model(inputs=[inputMessage, inputValue, inputDT],
                           outputs=[outputMessage, outputValue, outputDT])
+
--- train/sliceData.py
+++ train/sliceData.py
@@ -12,9 +12,9 @@ slicedDataValues = []
 slicedDataDTs = []
 
 number = 0
-length = 50
+length = 2000
 for messages, values, DTs in zip(dataMessages, dataValues, dataDTs):
-    if number <= 100000:
+    if number <= 10000:
         for i in range(len(messages)//length):
             number += 1
             print(number, end="\r")
